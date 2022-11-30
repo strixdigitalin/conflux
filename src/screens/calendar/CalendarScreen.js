c4931037d6675e5451ca57d1e5af47f729b1bd05
lakheraakshay <akshay.lakhera@easystepin.com>
2022-11-30 14:00:02 +0530

    markers

--- src/screens/calendar/CalendarScreen.js
+++ src/screens/calendar/CalendarScreen.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Text, FlatList} from 'react-native';
 
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import moment from 'moment';
-import { COLORS, SIZES } from '../../utils/theme';
-import { CalendarLoader } from './CalendarLoader';
-import { Image } from 'react-native';
+import {COLORS, SIZES} from '../../utils/theme';
+import {CalendarLoader} from './CalendarLoader';
+import {Image} from 'react-native';
 import PayslipHeader from '../payslip/PayslipHeader';
-import { commonStyles } from '../../utils/styles';
-import { getAttedance } from '../../utils/API';
-import { useSelector } from 'react-redux';
-const a1 = { key: 'a1', color: 'black', selectedDotColor: 'blue' };
-const a2 = { key: 'a2', color: 'black' };
-const a3 = { key: 'a3', color: 'black', selectedDotColor: 'blue' };
+import {commonStyles} from '../../utils/styles';
+import {getAttedance} from '../../utils/API';
+import {useSelector} from 'react-redux';
+const a1 = {key: 'a1', color: 'black', selectedDotColor: 'blue'};
+const a2 = {key: 'a2', color: 'black'};
+const a3 = {key: 'a3', color: 'black', selectedDotColor: 'blue'};
 var today = new Date();
 var date =
   today.getFullYear() +
@@ -69,10 +69,10 @@ function getMonthYear(month, year) {
   return console.log('Returned monthyear: ', monthyear), monthyear;
 }
 
-export default function CalendarScreen({ navigation, route }) {
+export default function CalendarScreen({navigation, route}) {
   console.log(userData, '<<<<');
   var month = parseInt(moment().format('MM'));
-  const { userData } = useSelector(state => state.User);
+  const {userData} = useSelector(state => state.User);
   const [attandance, setattandance] = useState([]);
   var [currMonthPassed, setCurrMonthPassed] = useState(
     moment().format('YYYY-MM-DD'),
@@ -120,7 +120,7 @@ export default function CalendarScreen({ navigation, route }) {
       console.log('forech', element.CALENDAR_DATE);
       var d = moment(element.CALENDAR_DATE).format('yyyy-MM-DD');
       console.log('d', d);
-      dateArray[d] = { dots: [a2] };
+      dateArray[d] = {dots: [a2]};
     });
 
     setDates(dateArray);
@@ -135,82 +135,185 @@ export default function CalendarScreen({ navigation, route }) {
     }
   });
 
-  const [markedDates, setMarkedDates] = useState({
-    '2022-12-20': { textColor: 'green' },
-    '2021-12-22': { startingDay: true, color: 'green' },
-    '2021-12-23': { selected: true, endingDay: true, color: 'green', textColor: 'gray' },
-    '2021-12-04': { disabled: true, startingDay: true, color: 'green', endingDay: true }
-  })
-
-  const handleDayPress = (day) => {
-    setMarkedDates({
-      [day.dateString]: {
-        startingDay: true, color: 'green'
-      },
-      [moment(day.dateString).add(1, 'days').format('YYYY-MM-DD')]: {
-        color: 'green'
-      },
-      [moment(day.dateString).add(2, 'days').format('YYYY-MM-DD')]: {
-        color: 'green'
-      },
-      [moment(day.dateString).add(3, 'days').format('YYYY-MM-DD')]: {
-        endingDay: true, color: 'green'
-      }
-    })
-  }
-
   return (
     <>
+      {/* <NavBar navigation={navigation} title="My Calendar" /> */}
       <PayslipHeader navigation={navigation} />
       <ScrollView>
         {loading ? (
           <CalendarLoader />
         ) : (
           <View>
-            <Text style={styles.attendanceCalendarText}>
+            {/* <ScrollView> */}
+            {/* <View style={{ pointerEvents: "none", backgroundColor: 'transparent' }}> */}
+            <Text
+              style={{
+                ...commonStyles.fs15_400,
+                color: '#0073FF',
+                margin: 20,
+                textAlign: 'center',
+              }}>
               Attendance Calendar
             </Text>
             <CalendarList
               current={currMonthPassed}
               horizontal={true}
               // hideArrows={false}
-              onDayPress={handleDayPress}
               pagingEnabled={true}
               calendarWidth={SIZES.width}
-              // markingType={'dot'}
-              theme={{ todayTextColor: 'black' }}
+              markingType={'multi-dot'}
+              theme={{todayTextColor: 'black'}}
               enableSwipeMonths={true}
-              disableIntervalMomentum
-              markedDates={markedDates}
-              markingType={'period'}
+              markedDates={{
+                '2022-28-11': {
+                  selected: true,
+                  marked: true,
+                  selectedColor: 'blue',
+                },
+                '2012-05-17': {marked: true},
+                '2012-05-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+                '2012-05-19': {disabled: true, disableTouchEvent: true},
+              }}
+              onDayPress={day => {
+                console.log('selected day', day);
+              }}
             />
+            <View
+              style={{
+                width: SIZES.width,
+                height: 365,
+                marginTop: -365,
+                backgroundColor: 'transparent',
+              }}>
+              <View style={{width: SIZES.width, padding: 20}}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      var val = moment(currMonthPassed);
+                      val = val.subtract(1, 'months').format('YYYY-MM-DD');
+                      setCurrMonthPassed(val);
+                      var valmonth = parseInt(moment(val).format('MM'));
+                      setCurrentMonth(valmonth);
+                      console.log('setCurrMonthPassed Left', val, valmonth);
+                    }}>
+                    <View style={{height: 30, width: 40, alignItems: 'center'}}>
+                      <Image
+                        source={require('../../assets/img/c-back.png')}
+                        style={{width: 20, height: 20}}
+                      />
+                    </View>
+                  </TouchableOpacity>
 
-            <Text style={styles.eventsText}>Events</Text>
+                  <TouchableOpacity
+                    onPress={() => {
+                      var val = moment(currMonthPassed);
+                      val = val.add(1, 'months').format('YYYY-MM-DD');
+                      setCurrMonthPassed(val);
+                      var valmonth = parseInt(moment(val).format('MM'));
+                      setCurrentMonth(valmonth);
+                      console.log('setCurrMonthPassed Right', val, valmonth);
+                    }}>
+                    <View style={{height: 30, width: 40, alignItems: 'center'}}>
+                      <Image
+                        source={require('../../assets/img/c-next.png')}
+                        style={{width: 20, height: 20}}
+                      />
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            {/* </View> */}
+
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: '600',
+                marginLeft: 10,
+                marginTop: 20,
+                fontFamily: 'STCForward-Regular',
+                color: '#000',
+                marginBottom: 10,
+              }}>
+              {' '}
+              Events{' '}
+            </Text>
 
             {true > 0 ? (
               <FlatList
                 // data={[1, 2]}
                 data={attandance}
-                renderItem={({ item, id }) => {
+                renderItem={({item, id}) => {
+                  var bgColor = '#E8EBFB';
+
+                  var d = moment(item.CALENDAR_DATE).format('DD');
+                  //
+                  // return (
+                  //   <View
+                  //     style={[
+                  //       styles.leaveShowContainer,
+                  //       {backgroundColor: bgColor},
+                  //     ]}
+                  //     key={id}>
+                  //     <View style={{...commonStyles.rowBetween}}>
+                  //       <View style={{...commonStyles.rowStart}}>
+                  //         <Text
+                  //           style={{
+                  //             ...commonStyles.fs16_700,
+                  //             color: '#1C67F6',
+                  //           }}>
+                  //           {/* {item} */}
+                  //         </Text>
+                  //         <Image
+                  //           source={require('../../assets/img/check.png')}
+                  //           style={{width: 12, height: 12, marginLeft: 4}}
+                  //         />
+                  //       </View>
+                  //       <Text style={{...commonStyles.fs12_400}}>
+                  //         {item.atten_date}
+                  //       </Text>
+                  //     </View>
+                  //     <Text style={{...commonStyles.fs14_500}}>
+                  //       Financial Year: {item.financial_year}
+                  //     </Text>
+                  //     <Text style={{...commonStyles.fs14_500}}>
+                  //       Status: {item.status}
+                  //     </Text>
+                  //   </View>
+                  // );
+
                   return (
                     <EventCard
                       count={10}
                       head={item.financial_year}
                       subhead={item.status}
                       from={item.atten_date}
+                      // to={'item.EVENT_END_DATE'}
                       key={item}
+                      // item={item}
                     />
                   );
                 }}
                 keyExtractor={item => item.id}
               />
             ) : (
-              <Text style={styles.noEventsText}>
+              <Text
+                style={{
+                  alignContent: 'center',
+                  alignItems: 'center',
+                  alignSelf: 'center',
+                  color: 'grey',
+                  marginTop: 100,
+                }}>
                 No Events for this month
               </Text>
             )}
 
-            <View style={{ height: 100 }} />
+            <View style={{height: 100}} />
           </View>
         )}
       </ScrollView>
@@ -218,27 +321,27 @@ export default function CalendarScreen({ navigation, route }) {
   );
 }
 
-function EventCard({ count, head, from, to, subhead, item }) {
+function EventCard({count, head, from, to, subhead, item}) {
   return (
     <View style={styles.userBlock}>
-      <View style={{ width: SIZES.width - 45, paddingLeft: 2 }}>
-        <Text style={{ marginBottom: 2, ...commonStyles.fs15_600 }}>
+      <View style={{width: SIZES.width - 45, paddingLeft: 2}}>
+        <Text style={{marginBottom: 2, ...commonStyles.fs15_600}}>
           Financial Year {head}
         </Text>
-        <View style={{ ...commonStyles.rowStart }}>
-          <Text style={{ ...commonStyles.fs13_400, marginBottom: 2 }}>
+        <View style={{...commonStyles.rowStart}}>
+          <Text style={{...commonStyles.fs13_400, marginBottom: 2}}>
             Status
           </Text>
           <Text
-            style={{ ...commonStyles.fs13_500, marginBottom: 2, marginLeft: 8 }}>
+            style={{...commonStyles.fs13_500, marginBottom: 2, marginLeft: 8}}>
             {subhead}
           </Text>
         </View>
 
-        <View style={{ ...commonStyles.rowStart }}>
-          <Text style={{ ...commonStyles.fs13_400, marginBottom: 2 }}>Time</Text>
+        <View style={{...commonStyles.rowStart}}>
+          <Text style={{...commonStyles.fs13_400, marginBottom: 2}}>Time</Text>
           <Text
-            style={{ ...commonStyles.fs13_500, marginBottom: 2, marginLeft: 8 }}>
+            style={{...commonStyles.fs13_500, marginBottom: 2, marginLeft: 8}}>
             {from}
           </Text>
         </View>
@@ -306,26 +409,4 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     backgroundColor: 'black',
   },
-  eventsText: {
-    fontSize: 20,
-    fontWeight: '600',
-    marginLeft: 10,
-    marginTop: 20,
-    fontFamily: 'STCForward-Regular',
-    color: '#000',
-    marginBottom: 10,
-  },
-  noEventsText: {
-    alignContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    color: 'grey',
-    marginTop: 100,
-  },
-  attendanceCalendarText: {
-    ...commonStyles.fs15_400,
-    color: '#0073FF',
-    margin: 20,
-    textAlign: 'center',
-  }
 });
